7838114f40f287801047e991e8d2936c5635d23a
IstsHitori <sernaojuliofranciscojavier@gmail.com>
2025-11-13 07:18:49 -0500

    Adding entities to patient.module

--- src/patient/patient.module.ts
+++ src/patient/patient.module.ts
@@ -2,15 +2,22 @@ import { Module } from '@nestjs/common';
 import { PatientService } from './patient.service';
 import { PatientController } from './patient.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Condition, CurrentMedication, Patient } from './entities';
-import { FamilyMember } from './entities/family-member.entity';
-import { FamilyMemberBackgrounds } from './entities/family-member-backgrounds.entity';
-import { FamilyBackgrounds } from './entities/family-backgrounds.entity';
-import { SymptomsPresent } from './entities/symptoms-present.entity';
-import { CognitiveEvaluation } from './entities/cognitive-evaluation.entity';
 import { AuthModule } from 'src/auth/auth.module';
 import { ActivityModule } from 'src/activity/activity.module';
 import { Recent_Activity } from 'src/activity/entities/recent-activity.entity';
+import {
+  CognitiveEvaluation,
+  Condition,
+  CurrentMedications,
+  Eps,
+  EpsRegime,
+  FamilyBackgrounds,
+  HealthyRegime,
+  Patient,
+  PatientCondition,
+  PatientCurrentMedications,
+  SymptomsPresent,
+} from './entities';
 
 @Module({
   controllers: [PatientController],
@@ -19,12 +26,15 @@ import { Recent_Activity } from 'src/activity/entities/recent-activity.entity';
     TypeOrmModule.forFeature([
       Patient,
       Condition,
-      CurrentMedication,
-      FamilyMember,
-      FamilyMemberBackgrounds,
       FamilyBackgrounds,
       SymptomsPresent,
       CognitiveEvaluation,
+      Eps,
+      HealthyRegime,
+      EpsRegime,
+      PatientCondition,
+      CurrentMedications,
+      PatientCurrentMedications,
       Recent_Activity,
     ]),
     AuthModule,
