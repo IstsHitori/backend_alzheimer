4556643a90a6a872b19b5ecf27fe1033343dd009
IstsHitori <sernaojuliofranciscojavier@gmail.com>
2025-12-10 16:20:02 -0500

    project finished

--- src/analysis/constants/data-enums.ts
+++ src/analysis/constants/data-enums.ts
@@ -1,6 +1,6 @@
 export enum DIAGNOSIS {
   NO_DEMENTED = 'No Demente',
-  VERY_MILD_DEMENTED = 'Alzheimer Leve',
-  MILD_DEMENTED = 'Alzheimer Moderado',
+  VERY_MILD_DEMENTED = 'Alzheimer Muy Leve',
+  MILD_DEMENTED = 'Alzheimer Leve',
   MODERATE_DEMENTED = 'Alzheimer Severo',
 }
